26d8b74aae7a0d39b4e77e40c602d1e607fda874
Maria Turcan <Maria.Mustea@endava.com>
2021-01-29 18:55:18 +0200

    added scenario for register api

---
+++ MPP.Acceptance.Test.API.Specs/Model/RegisterUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace MPP.Acceptance.Test.API.Specs.Model
+{
+    [DataContract]
+    public class RegisterUserRequest
+    {
+        [DataMember(Name = "email")]
+        public string Email { get; set; }
+
+        [DataMember(Name = "password")]
+        public string Password { get; set; }
+    }
+
+}
--- MPP.Acceptance.Test.API.Specs/Steps/RowObjects.cs
+++ MPP.Acceptance.Test.API.Specs/Steps/RowObjects.cs
@@ -3,4 +3,6 @@
 namespace MPP.Acceptance.Test.API.Specs.Steps
 {
    public record CreateUserRowObject(string Name, string Job);
+
+   public record RegisterUserRowObject(string Email, string Password);
 }
